a102c155e266603cae4fce5d385db5ed475b324f
lukianov <lukyanovvitaliymailbox@gmail.com>
2023-03-27 00:28:32 +0300

    build: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/formula-racers-info/'
+  base: '/formula-racers-info/',
+  resolve: {
+    alias: {
+      '@': path.resolve('./src/components'),
+      'racersImages': path.resolve('./assets/racersImages')
+    }
+  }
 })
